61aa8255ca0ca2b9980dc980edb255b5de838b93
WangLei <wangl_msg@163.com>
2022-07-31 17:34:38 +0800

    404页面布局

--- src/page/404.tsx
+++ src/page/404.tsx
@@ -1,3 +1,47 @@
-export default function NotFound() {
-  return <h1>对不起，您访问的页面未找到。</h1>;
+import { Button, Result, Row, Col } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+export default function NoFound() {
+  const navigate = useNavigate();
+  return (
+    <div
+      style={{
+        width: '100vw',
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Result
+        status="404"
+        title="404"
+        subTitle="抱歉，您访问的页面不存在。"
+        extra={
+          <Row gutter={50} justify="center">
+            <Col>
+              <Button
+                type="primary"
+                onClick={() => {
+                  navigate(-1);
+                }}
+              >
+                返回上一页
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                type="primary"
+                onClick={() => {
+                  navigate('/');
+                }}
+              >
+                返回首页
+              </Button>
+            </Col>
+          </Row>
+        }
+      />
+    </div>
+  );
 }
