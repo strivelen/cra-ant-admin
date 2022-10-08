1e7b741e8040aaed247f98374ce072c7e54e9070
Len <wangl_msg@163.com>
2022-10-08 17:12:59 +0800

    优化DatePicker组件

--- src/component/FormItem/DatePicker.tsx
+++ src/component/FormItem/DatePicker.tsx
@@ -1,7 +1,23 @@
-import { Dayjs } from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import dayjsGenerateConfig from 'rc-picker/es/generate/dayjs';
 import generatePicker from 'antd/es/date-picker/generatePicker';
 
-const DatePicker = generatePicker<Dayjs>(dayjsGenerateConfig);
+export const DayJsDatePicker = generatePicker<Dayjs>(dayjsGenerateConfig);
 
-export default DatePicker;
+export default function DatePicker({
+  value,
+  onChange,
+  ...otherProps
+}: {
+  value: string;
+  onChange: (val: string) => void;
+}) {
+  return (
+    <DayJsDatePicker
+      allowClear
+      value={dayjs(value)}
+      onChange={(val) => onChange && onChange(dayjs(val).format('YYYY-MM-DD'))}
+      {...otherProps}
+    />
+  );
+}
